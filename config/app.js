a2147719e8b46f1d49701012bde3c3db3a28b5aa
Richard <richardchachalo03@gmail.com>
2025-05-19 22:03:16 -0500

    Agregado sistema de chat en tiempo real con Socket.IO sin DB

--- config/app.js
+++ config/app.js
@@ -3,11 +3,21 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import loginRoutes from './../controllers/routes/loginRoutes.js'; // Ruta de login/registro
 import connection from './db.js'; // Conexión a la base de datos (db.js)
-
+import logger from 'morgan';
+import { Server } from 'socket.io'; // Socket.io para la comunicación en tiempo real
+import { createServer } from 'node:http'; // Crear servidor HTTP
 dotenv.config();
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery:true
+});
+
 const PORT = process.env.PORT || 3000;
+
+// ✅ SERVIR ARCHIVOS ESTÁTICOS DESDE LA CARPETA PUBLIC
+app.use(express.static(process.cwd() + '/public'));
 
 // Configuración de CORS (permitir acceso desde Netlify o cualquier frontend que lo necesite)
 const allowedOrigins = [
@@ -30,15 +40,39 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// Rutas API
+// Rutas API2
 app.use('/api', loginRoutes);
 
-// Ruta de prueba
+//Cuando se conecta un cliente chat
+io.on('connection',(socket) =>{
+  console.log('Usuario conectado')
+  //Cuando se desconecta un cliente
+  socket.on('disconnect', () =>{
+    console.log('Usuario desconectado')
+  })
+
+  //Cuando se envía un mensaje
+  socket.on('chat mensaje', (msg)=>{
+    io.emit('chat mensaje', msg)
+  })
+
+})
+
+// Escuchar peticiones HTTP
+app.use(logger('dev'));
+
+
+//Ruta de Chat
+app.get('/chat',(req, res ) => {
+  res.sendFile(process.cwd() +'/views/inicio_sesion/Chat.html');    
+});
+
+//Ruta de prueba
 app.get('/', (req, res) => {
   res.json({ message: 'Backend funcionando correctamente' });
 });
 
 // Inicio del servidor
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`🚀 Servidor backend corriendo en http://localhost:${PORT}`);
 });
